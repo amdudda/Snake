d0f9501cec030c6d5fc8f61051be615e008c5f6c
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-21 14:52:15 -0500

    Refactored java classes to com.amdudda

--- com/amdudda/SnakeGame.java
+++ com/amdudda/SnakeGame.java
@@ -1,4 +1,4 @@
-package com.clara;
+package com.amdudda;
 
 import java.util.Timer;
 
@@ -27,7 +27,7 @@ public class SnakeGame {
 	static final int GAME_WON = 4;   //The values are not important. The important thing is to use the constants 
 	//instead of the values so you are clear what you are setting. Easy to forget what number is Game over vs. game won
 	//Using constant names instead makes it easier to keep it straight. Refer to these variables 
-	//using statements such as SnakeGame.GAME_OVER 
+	//using statements such as SnakeGame.GAME_OVER
 
 	private static int gameStage = BEFORE_GAME;  //use this to figure out what should be happening. 
 	//Other classes like Snake and DrawSnakeGamePanel will need to query this, and change it's value
